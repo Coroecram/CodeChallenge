0f3c8803dc945d748782c117ece9b73a8bae4ad7
michael.budnick <michael.budnick@outlook.com>
2016-01-10 17:01:26 -0500

    Fill out Event associations

--- MiniEventBrite/app/models/event.rb
+++ MiniEventBrite/app/models/event.rb
@@ -1,2 +1,5 @@
 class Event < ActiveRecord::Base
+  has_many :invitations
+  has_many :invited,   -> {where(confirmation: false)}, class_name: 'Person', through: :invitations
+  has_many :attendees, -> {where(confirmation: true)},  class_name: 'Person', through: :invitations
 end
