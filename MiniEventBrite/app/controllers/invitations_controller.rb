100f3716cae5c84583190a1e10aaf22874bf7044
michael.budnick <michael.budnick@outlook.com>
2016-01-10 17:22:14 -0500

    Fill out invitation controller

--- MiniEventBrite/app/controllers/invitations_controller.rb
+++ MiniEventBrite/app/controllers/invitations_controller.rb
@@ -1,3 +1,44 @@
 class InvitationsController < ApplicationController
-  
+  def new
+  @invitation = Invitation.new
+    render :new
+  end
+
+  def create
+    @invitation = Invitation.new(invitation_params)
+    @invitation.user_id = current_user.id
+
+    if @invitation.save
+      redirect_to event_url(@invitation.event_id)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @invitation = Invitation.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @invitation = Invitation.find(params[:id])
+
+    if @invitation.update(invitation_params)
+      redirect_to event_url(@invitation.event_id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @invitation = Invitation.find(params[:id])
+    Inviation.destroy(@invitation)
+    redirect_to root_url
+  end
+
+  private
+  def invitation_params
+    params.require(:invite).permit(:event_id, :person_id)
+  end
+
 end
