8635f8b21cbe3841113f24f401907712c686942b
michael.budnick <michael.budnick@outlook.com>
2016-01-10 17:13:40 -0500

    Add Event validations and custom validations for TBD date

--- MiniEventBrite/app/models/event.rb
+++ MiniEventBrite/app/models/event.rb
@@ -2,4 +2,13 @@ class Event < ActiveRecord::Base
   has_many :invitations
   has_many :invited,   -> {where(confirmation: false)}, class_name: 'Person', through: :invitations
   has_many :attendees, -> {where(confirmation: true)},  class_name: 'Person', through: :invitations
+
+  validates :name, presence: true, uniqueness: { scope: :date }
+  validates :date, uniqueness: { scope: :name }
+  validate :date_or_to_be_decided
+
+  private
+    def date_or_to_be_decided
+      date ||= DateTime.new(1675, 3, 4)
+    end
 end
